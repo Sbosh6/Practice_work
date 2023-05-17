77ee1f9ecabf13df4061b0a0a9f1deb3b61c0fd8
Sbosh6 <sbonisongubane492@gmail.com>
2023-05-17 12:46:33 +0000

    Attempts on Functions

---
+++ main.py
@@ -0,0 +1,49 @@
+#def greet_friend():
+#print("Hello guys hope you all good")
+#people = ["Sphe", "Luno", "Ray"]
+
+#for i in people:
+#print(i)
+#if i == "Ray":
+#break
+#continue
+#print(i)
+
+#greet_friend()
+#def prime_number(list):
+
+#for x in range(0, 25):
+#x == 12
+#while True:
+#print(x)
+#else:
+#print("Dammn you can't do that")
+#print(x)
+#if x >= 10 and x <= 15:
+#print(x)
+#break
+
+#print(x)
+#for x in range(2, 10, 2):
+  #if x == 2 and x == 8:
+    #print("Yeah the 2 expensive nos!")
+  #else:
+    #print("This is for you mate")
+  #print(x)
+colours = ["red", "white", "blue"]
+brands = ["Toyota", "BMW", "Audi"]
+car = ["Fortuna", "M3", "A5"]
+for x in colours:
+  if x == "red":
+    print(x)
+  for y in brands:
+    if y == "BMW":
+      print(y)
+    for z in car:
+      if z == "M3":
+        print()
+      print(x, y, z)
+      
+  
+
+    
